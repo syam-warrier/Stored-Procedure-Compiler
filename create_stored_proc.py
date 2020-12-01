e18656302f3195a86ebb93983ddbf2e2f4662cf9
syam-warrier <46114725+syam-warrier@users.noreply.github.com>
2020-12-02 00:06:01 +0530

    Add files via upload

--- create_stored_proc.py
+++ create_stored_proc.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     spec = pd.read_excel(sys.argv[1], sheet_name=sys.argv[2]).fillna('')
     spec = spec[spec["Appl ID"] == sys.argv[3]]
     spec = spec[spec["Target Table Name.1"] == sys.argv[4]]
-    spec = spec[(spec["Data Store Name"] == sys.argv[5]) | (spec["Business Logic"] == "")]
+    spec = spec[(spec["Data Store Name"] == sys.argv[5]) | (spec["Stage Table"] == "")]
     spec["Data Store Name"] = sys.argv[5]
     spec = spec[spec["Derived/Verbati?"] != "Not Needed"]
     
